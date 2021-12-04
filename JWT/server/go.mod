81accdf7c5f8a86c211e2b64a5c6ad1e402fbbf6
Tarun Bansal <45669002+tarun4all@users.noreply.github.com>
2021-12-04 11:37:44 +0530

    Add files via upload

---
+++ JWT/server/go.mod
@@ -0,0 +1,5 @@
+module github.com/elliotforbes/go-jwt-token/server
+
+go 1.13
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
---
+++ JWT/server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+var mySigningKey = []byte("shhhhhhhhhhhh")
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Protected Page")
+}
+
+// Middleware
+func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["Token"] != nil {
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an Error")
+				}
+				return mySigningKey, nil
+			})
+
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
+
+			if token.Valid {
+				endpoint(w, r)
+			}
+		} else {
+			fmt.Fprintf(w, "Not Authorized")
+		}
+	})
+}
+
+func createServer() {
+	http.Handle("/", isAuthorized(homePage))
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func main() {
+	fmt.Println("Server")
+
+	createServer()
+}
